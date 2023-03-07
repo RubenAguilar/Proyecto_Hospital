dafebdad1ad510a4bd7be66e2f6b06778dd61e62
Ruben Aguilar <lapiedrita@MacBook-Pro-de-Ruben.local>
2023-03-06 23:10:14 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <title>Pagina principal</title>
+  </head>
+  <body>
+    <header>
+     
+    <div class="menu">
+                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark menu">
+                    <a class="nav-link disabled">INICIO</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                      <div class="navbar-nav">
+                      <a class="nav-link disabled">CITAS</a>
+                      <a class="nav-link active" href="Contacto.php">CONTACTO</a>
+                      
+                      </div>
+                    </div>
+                    <form class="form-inline">
+                    <a class="btn btn-outline-success" href="inicio_sesion.php">Iniciar sesion</a>
+                    <a class="btn btn-outline-success" href="eleccion.php">Registrarse</a>
+                      </form>
+                  </nav>
+                  </div>
+          </header>
+          <br>
+    <main>
+      <div class="container-fluid"> 
+        <div class="row">
+          <div class="col-1"></div>
+          <div class="col-10">
+                    <div id="carouselExampleSlidesOnly " class="carousel slide " data-ride="carousel">
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="img/hospital-fachada.jpeg" class="d-block w-20" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                      <img src="img/hospital-sala.jpeg" class="d-block w-20">
+                    </div>
+                    <div class="carousel-item">
+                      <img src="img/hospital-cuarto.jpeg" class="d-block w-20" alt="...">
+                    </div>
+                  </div>
+                </div>
+           </div>
+           <div class="col-1"></div>
+         </div>     
+      </div>
+
+          <div class="container">
+            
+             
+            <div class="card text-center">
+              <div class="card-header">
+               
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Bienvenido</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quisquam vero est obcaecati iure sint nulla? Qui veniam fugit placeat quibusdam, officiis nemo nesciunt aliquid, eius minima facilis eveniet a.
+                Expedita, quae aliquid consequatur perferendis, sint, recusandae explicabo adipisci laudantium magnam doloribus eveniet dolores deleniti pariatur fuga. Minus laudantium quidem, voluptates facilis quisquam aperiam error porro dolore. Voluptatum, vel veritatis.</p>
+                <a href="#" class="btn btn-primary">Registrate</a>
+              </div>
+              <div class="card-footer text-muted">
+                
+               </div>
+              </div>
+          
+         
+
+    </main>
+
+    
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+   
+   
+  </body>
+</html>
---
+++ inicio_sesion.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="css/style.css"> 
+    <title>Inicio de sesion</title>
+</head>
+<body>
+<header>
+     
+     <div class="menu">
+                     <nav class="navbar navbar-expand-lg navbar-dark bg-dark menu">
+                     <a class="nav-link active" href="index.php">INICIO</a>
+                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                       <span class="navbar-toggler-icon"></span>
+                     </button>
+                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                       <div class="navbar-nav">
+                       <a class="nav-link disabled">CITAS</a>
+                       <a class="nav-link active" href="Contacto.php">CONTACTO</a>
+                       
+                       </div>
+                     </div>
+                     <form class="form-inline">
+                     <a class="btn btn-outline-success" href="eleccion.php">Registrarse</a>
+                     
+                       </form>
+                   </nav>
+                   </div>
+           </header>
+           <br>
+           <main>
+            <div class="container">
+                    <div class="card">
+                            <h5 class="card-header">Ingresa los datos correspondientes</h5>
+                            <div class="card-body">
+                            <form>
+                                <div class="form-group">
+                            <label for="exampleUsuario">Usuario</label>
+                            <input type="text" class="form-control" id="Usuario" aria-describedby="UsuarioHelp" placeholder="Escribe tu usuario" >
+                            
+                        </div>
+                                    
+                       
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Contraseña</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Contraseña">
+                        </div>
+ 
+                        </form>
+                        <br>
+                                <a href="#" class="btn btn-primary">Iniciar</a>
+                            </div>
+                            </div>
+                            </div>
+           </main>
+</body>
+</html>
